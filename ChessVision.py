c300059040c83f2e90d05bb71464c15fd6a61382
Brian Oribe <brainoribe123@gmail.com>
2022-08-20 12:04:36 -0400

    Chess Vision
    Screenshot correctio

--- ChessVision.py
+++ ChessVision.py
@@ -1,14 +1,16 @@
 import cv2
-import numpy
-
+import numpy as np
+import pyautogui
 # this is where the detection will be handled will deliver 4 char str into main 
 class ChessVision:
     def __init__(self):
         pass
 
-    def get_screenshot(self,x,y):
+    def get_screenshot(self):
         while True:
-            screenshot = None
+            screenshot = pyautogui.screenshot()
+            screenshot = np.array(screenshot)
+            screenshot = screenshot[:, :, :, ::-1].copy()
             cv2.imshow('ChessVisor', screenshot)
             if cv2.waitKey(1) ==ord('q'):
                 break
@@ -16,5 +18,5 @@ class ChessVision:
         print('done')
 
 
-CV = ChessVision
+CV = ChessVision()
 CV.get_screenshot()
